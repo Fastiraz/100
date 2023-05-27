19c5db21d56863da4753b0a1394a1771b9829e93
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2023-05-27 20:29:30 +0200

    Update 100.cpp
    Adding greek chars and output file.

--- 100.cpp
+++ 100.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream> // Include the necessary header for file operations
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -6,47 +7,36 @@
 
 using namespace std;
 
-void generate(char* arr, int i, string s, int len, bool verbose)
+void generate(char* arr, int i, string s, int len, bool verbose, ofstream& outputFile)
 {
     if (i == 0) {
         if (verbose)
             cout << s << endl;
 
-        if (s == "azb") {
-            cout << "MATCH!: " << s << endl;
-            exit(0);
-        }
+        // Write to the output file if it is open
+        if (outputFile.is_open())
+            outputFile << s << endl;
+
         return;
     }
 
     for (int j = 0; j < len; j++) {
 
         string appended = s + arr[j];
-        generate(arr, i - 1, appended, len, verbose);
+        generate(arr, i - 1, appended, len, verbose, outputFile);
     }
 
     return;
 }
 
-void crack(char* arr, int len, bool verbose) {
+void crack(char* arr, int len, bool verbose, ofstream& outputFile) {
     for (int i = 1; i <= len; i++) {
-        generate(arr, i, "", len, verbose);
+        generate(arr, i, "", len, verbose, outputFile);
     }
 }
 
 int main(int argc, char* argv[]) {
     chrono::high_resolution_clock::time_point start_time = chrono::high_resolution_clock::now();
-
-    /*char arr[] = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-',
-        '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^',
-        '_', '`', '{', '|', '}', '~'
-    };*/
 
     bool verbose = false;
     string lowercase = "abcdefghijklmnopqrstuvwxyz";
@@ -59,7 +49,10 @@ int main(int argc, char* argv[]) {
     string chinese = "你好世界";
     string korean = "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㄲㄸㅃㅆㅉㄳㄵㄶㄺㄻㄼㄽㄾㄿㅀㅄ";
     string hindi = "अआइईउऊऋएऐओऔकखगघचछजझटठडढणतथदधनपफबभमयरलवशषसह";
+    string greek = "αβγδεζηθικλμνξοπρστυφχψω";
     string charset;
+
+    ofstream outputFile; // Declare the output file stream
 
     for (int i = 1; i < argc; i++) {
         if (string(argv[i]) == "-l" || string(argv[i]) == "--lowercase") {
@@ -82,17 +75,38 @@ int main(int argc, char* argv[]) {
             charset += korean;
         } else if (string(argv[i]) == "-h" || string(argv[i]) == "--hindi") {
             charset += hindi;
+	    } else if (string(argv[i]) == "-g" || string(argv[i]) == "--greek") {
+	        charset += greek;
         } else if (string(argv[i]) == "-v" || string(argv[i]) == "--verbose") {
             verbose = true;
+        } else if (string(argv[i]) == "-o" || string(argv[i]) == "--output") {
+            if (i + 1 < argc) {
+                string outputFilename = argv[i + 1];
+                outputFile.open(outputFilename);
+
+                if (!outputFile.is_open()) {
+                    cerr << "Error: Unable to open the output file." << endl;
+                    return 1;
+                }
+
+                i++; // Skip the next argument
+            } else {
+                cerr << "Error: Missing filename for the output file." << endl;
+                return 1;
+            }
         }
     }
-
 
     char arr[charset.length() + 1];
     strcpy(arr, charset.c_str());
 
     int len = strlen(arr);
-    crack(arr, len, verbose);
+    crack(arr, len, verbose, outputFile);
+
+    // Close the output file if it was opened
+    if (outputFile.is_open()) {
+        outputFile.close();
+    }
 
     chrono::high_resolution_clock::time_point end_time = chrono::high_resolution_clock::now();
     chrono::nanoseconds duration = chrono::duration_cast<chrono::nanoseconds>(end_time - start_time);
